e8d990c895970dd8cdd77c53ef7cc8503b038c6a
Mehakdeep kaur <Kaur9155sheridanc.on.ca>
2020-06-07 15:51:21 -0400

    second commit

--- src/exampletuesday/ExampleTuesday.java
+++ src/exampletuesday/ExampleTuesday.java
@@ -16,6 +16,7 @@ public class ExampleTuesday {
      */
     public static void main(String[] args) {
         System.out.println("Learn github");
+        System.out.println("second message");
     }
     
 }
